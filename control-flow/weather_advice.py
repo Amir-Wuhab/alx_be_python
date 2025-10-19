ad329a3e79a818d9354ce6ce9d9f21e25f6b10b7
AMIR WUHAB <amirowehab@gmail.com>
2025-10-19 21:48:36 +0800

    Update weather_advice.py

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -5,9 +5,7 @@ if weather=="sunny":
     print("Wear a t-shirt and sunglasses.")
 elif weather == "rainy":
     print("Don't forget your umbrella and a raincoat.")
-elif weather == "cold":
-    # This block runs if the user enters "cold".
+elif weather=="cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
-    # This block runs if the input does not match any of the above conditions.
     print("Sorry, I don't have recommendations for this weather.")
